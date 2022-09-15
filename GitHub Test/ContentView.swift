616a41ed89a1b899a89c24f9b9b29c8d066aaa6e
dvonnsimpson <ds3533@columbia.edu>
2022-09-15 12:43:29 -0400

    Initial Commit

---
+++ GitHub Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHub Test
+//
+//  Created by Donovan Simpson on 9/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHub Test/GitHub_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_TestApp.swift
+//  GitHub Test
+//
+//  Created by Donovan Simpson on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
